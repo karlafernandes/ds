6f3819a5c1c98106172959fa4b328755f208e02c
Bridge Tools <robot@bridgetools.io>
2021-12-03 16:11:13 +0200

    Container component

--- design-system/src/index.js
+++ design-system/src/index.js
@@ -11,6 +11,7 @@ export { colors, typography, spacing } from "./tokens";
 export { default as Avatar } from "./components/Avatar";
 export { default as Button } from "./components/Button";
 export { default as Carousel } from "./components/Carousel";
+export { default as Container } from "./components/Container";
 export { default as Hr } from "./components/Hr";
 export { default as Icon } from "./components/Icon";
 export { default as Navigation } from "./components/Navigation";
